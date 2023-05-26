765fec30dd11d5ae66ba5be4e1e785c8b92b333d
Franklin <fdoug2017@gmail.com>
2023-05-26 11:20:47 -0700

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import random
 
 class Suit(Enum):
     Spade = 0
@@ -14,44 +15,22 @@ class Card():
 
 
 NUM_OF_SUITS = 4
-
+suits = ["spades", "hearts", "diamonds", "clubs"]
 card_names = ["ace", "two", "three"]
-spades = []
-hearts = []
-diamonds = []
-clubs = []
-deck = [spades, hearts, diamonds, clubs]
-
-cards = []
+suit_names = ["_of_spades", "_of_hearts", "_of_diamonds", "_of_clubs"]
+deck = []
 for i in range(NUM_OF_SUITS):
     for card in card_names:
-        card = Card(card, i + 1, Suit(i))
-        if i == 0:
-            spades.append(card)
-        elif i == 1:
-            hearts.append(card)
-        elif i == 2:
-            diamonds.append(card)
-        elif i == 3:
-            clubs.append(card)
+        card += suit_names[i]
+        card = Card(card, i +1, Suit(i))
+        deck.append(card)
 
-print(deck[0])
-print(spades)
-print(hearts)
+#fix value
 
+spades = deck[:3]
+hearts = deck[3:6]
+diamonds = deck[6:9]
+clubs = deck[9:12]
 
-
-# spades = [ace, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen]
-# hearts = [ace, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen]
-# diamonds = [ace, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen]
-# clubs = [ace, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen]
-
-
-
-
-
-
-
-
-
-#Deck = [spades + hearts + diamonds + clubs]
+card1 = random.choice(deck)
+print(card1.name, card1.value, card1.suit)
