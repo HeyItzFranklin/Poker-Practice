7d0e347102d3b9d73cbe77f2e2e9515d5ea53bd3
Franklin <fdoug2017@gmail.com>
2023-05-30 11:46:59 -0700

    Backbone update
    Deck Building, Hand Strength Checking, and Displaying Cards to the Screen

--- cards.py
+++ cards.py
@@ -1,39 +1,41 @@
 from enum import Enum
 import random
+import pygame
+import os
+
+pygame.init()
 
 class Suit(Enum):
     Spade = 0
     Heart = 1
     Diamond = 2
-    Club= 3
+    Club = 3
+
 
 class Card():
-    def __init__(self, name, value, suit):
+    def __init__(self, name, value, suit, image):
         self.name = name
         self.value = value
         self.suit = suit
-        # Add image
-        # Add x pos
-        # Add y pos
+        self.image = image
+        self.x_pos = 0
+        self.y_pos = 0
 
+# class Hand():
+#     def __init__(self):
+#         self.cards = []   
 
-
-class Hand():
-    def __init__(self):
-        self.cards = []
-        
-        self.has_high_card = False
-        self.has_pair = False
-        self.has_two_pair = False
-        self.has_three_pair = False
-        self.has_straight = False
-        self.has_flush = False
-        self.has_full_house = False
-        self.has_four_of_a_kind = False
-        self.has_staight_flush = False
-        self.has_royal_flush = False
-        self.is_winning = False
-
+        # self.has_high_card = False
+        # self.has_pair = False
+        # self.has_two_pair = False
+        # self.has_three_pair = False
+        # self.has_straight = False
+        # self.has_flush = False
+        # self.has_full_house = False
+        # self.has_four_of_a_kind = False
+        # self.has_staight_flush = False
+        # self.has_royal_flush = False
+        # self.is_winning = False
 
 
 SUIT_NAMES = ["_of_spades", "_of_hearts", "_of_diamonds", "_of_clubs"]
@@ -41,16 +43,20 @@ NUM_OF_SUITS = len(SUIT_NAMES)
 CARD_NAMES = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
 CARDS_PER_SUIT = len(CARD_NAMES)
 
-
+# Addes the name of the card, the v(alue) of the card and the suit of the card to the deck
 deck = []
 for i in range(NUM_OF_SUITS):
     v = 1
     for card in CARD_NAMES:
         card += SUIT_NAMES[i]
-        card = Card(card, v, Suit(i))
+        image = card + ".png"
+        card = Card(card, v, Suit(i), pygame.transform.scale(pygame.image.load(os.path.join("Assets", "Card-Images", image)), (100, 200)))
+       
         deck.append(card)
         v += 1
 
+
+# Classifies the cards into their respective suits
 spades = []
 hearts = []
 diamonds  = []
@@ -66,6 +72,7 @@ for card in deck:
         clubs.append(card)
         
 
+# Deals cards and removes them from the deck
 def deal_card(location, num_of_cards) -> list:
     for i in range(num_of_cards):
         new_card = random.choice(deck)
@@ -73,25 +80,81 @@ def deal_card(location, num_of_cards) -> list:
         location.append(new_card)
     
     return location
+        
 
-def check_pairs(hero_hand):
+def display_cards(hero_hand, villian_hand, dealer_cards):
+    # Print Hero cards
+    print("HERO CARDS:")
+    for card in hero_hand:
+        print(card.name, card.value, card.suit)
+    print()
+
+    # Print Villian cards
+    print("VILLIAN CARDS:")
+    for card in villian_hand:
+        print(card.name, card.value, card.suit)
+    print()
+
+    # Print the table cards
+    if len(dealer_cards) == 3:
+        print("THE FLOP:")
+    elif len(dealer_cards) == 4:
+        print("THE TURN:")
+    elif len(dealer_cards) == 5:
+        print("THE RIVER:")
+    for card in dealer_cards:
+        print(card.name, card.value, card.suit)
+    print()
+    
+
+def sort_hand(whole_hand, original_hand, dealer_cards):
+    for card in original_hand:
+        whole_hand.append(card)
+
+    for card in dealer_cards:
+        whole_hand.append(card)
+
+    whole_hand = sorted(whole_hand, key=lambda card: card.value, reverse= True)
+    return whole_hand
+
+def check_pairs(whole_hand):
+    # NEED TO CHECK HOW STRONG THE PAIRS ARE
+
     # Check for pairs
     pairs = []
-    for i in range(len(hero_hand) - 1):
-        for j in range(i + 1, len(hero_hand)):
-            if hero_hand[i].value == hero_hand[j].value:
-                if hero_hand[i] not in pairs:
-                    pairs.append(hero_hand[i])
-                if hero_hand[j] not in pairs:
-                    pairs.append(hero_hand[j])
+    for i in range(len(whole_hand) - 1):
+        for j in range(i + 1, len(whole_hand)):
+            if whole_hand[i].value == whole_hand[j].value:
+                if whole_hand[i] not in pairs:
+                    pairs.append(whole_hand[i])
+                if whole_hand[j] not in pairs:
+                    pairs.append(whole_hand[j])
     
-    for pair in pairs:
-        print(pair.name)
+    # for pair in pairs:
+    #     print(pair.name)
 
-    # Distungish between pairs, 2 pair, 3 pair, 4 pair
-    # Full house
+    if len(pairs) == 2:
+        print("one pair")
+    elif len(pairs) == 3:
+        print("three of a kind")
+    elif len(pairs) == 4:
+        quad = True
+        for card in pairs:
+            if card.value != pairs[0].value:
+                quad = False
+                break
+        if quad == True:
+            print("four of a kind")
+        else:
+            print("2 pair")
+    elif len(pairs) == 5:
+        print("full house")
+
+
 
 def check_flush(hero_hand):
+    # NEED TO CHECK HOW STRONG THE FLUSH IS 
+
     # Check Flush
     has_flush = False
     hand_hearts = []
@@ -112,55 +175,83 @@ def check_flush(hero_hand):
         has_flush = True
         print("flush")
 
-    # gotta check flush high   
 
 def check_straight(hero_hand):
-    pass
-    # 5 over is amount to make a straight
-    # is threre a way to calculate 5 fits into 7 3 times
-    # check from first card to 5 over
-    # check from second card to 5 over
-    # check from third carrd to 5 over
+    # STILL NEED TO CATCH ACE KING CASE 
+    # STILL NEED TO CHECK HOW STRONG THE STRAIGHT IS
 
-            
+    # might be able to get chances without hard coding numbers not sure
 
+    chances = 1
+    if len(hero_hand) == 5:
+        chances = 1
+    elif len(hero_hand) == 6:
+        chances = 2
+    elif len(hero_hand) == 7:
+        chances == 3
     
-
+    straight_len = 5
+    straight = False
+    for i in range(chances):
+        if straight == False:
+            s = 1
+            for j in range(i + 1, i + straight_len):
+                if hero_hand[j].value == hero_hand[i].value - 1:
+                    i += 1
+                    s += 1
+                    if s == 5:
+                        straight = True
+                        break
+                else:
+                    break
+    
+    if straight == True:
+        print("straight")
 
 def check_hand_strength(hero_hand, villian_hand, dealer_cards):
-    for card in dealer_cards:
-        hero_hand.append(card)
-
-    hero_hand = sorted(hero_hand, key=lambda card: card.value, reverse= True)
+    whole_hero_hand = []
+    whole_villian_hand = []
+    whole_hero_hand = sort_hand(whole_hero_hand, hero_hand, dealer_cards)
+    whole_villian_hand = sort_hand(whole_villian_hand, villian_hand, dealer_cards)
     
-    # need to return something
-    check_pairs(hero_hand)
-    check_flush(hero_hand)
-    # check straight
+    print("Hero")
+    check_pairs(whole_hero_hand)
+    check_flush(whole_hero_hand)
+    check_straight(whole_hero_hand)
+    print()
+    print("villian")
+    check_pairs(whole_villian_hand)
+    check_flush(whole_villian_hand)
+    check_straight(whole_villian_hand)
+    print()
+    
+
+def deal_player(cards):
+    player = []
+    player = deal_card(player, cards)
+
+    return player
+    
+
+# display_cards(hero, villian, flop)
+# check_hand_strength(hero, villian, flop)
+
+# button for this
+# next_deal = str(input("Deal next card?: "))
+# if next_deal == "y":
+#     deal_turn(hero, villian, flop)
 
 
-hero = Hand()
-hero.cards = deal_card(hero.cards, 2)
 
-villian = Hand()
-villian.cards = deal_card(villian.cards, 2)
-
-flop = []
-flop = deal_card(flop, 3)
-
-print("HERO CARDS:")
-for card in hero.cards:
-    print(card.name, card.value, card.suit)
-print()
-print("VILLIAN CARDS:")
-for card in villian.cards:
-    print(card.name, card.value, card.suit)
-print()
-print("THE FLOP:")
-for card in flop:
-    print(card.name, card.value, card.suit)
-print()
-
-check_hand_strength(hero.cards, villian.cards, flop)
+def deal_turn(hero_hand, villian_hand, dealer_cards):
+    turn = deal_card(dealer_cards, 1)
+    display_cards(hero_hand, villian_hand, turn)
+    check_hand_strength(hero_hand, villian_hand, turn)
+    return turn
 
 
+def deal_river(hero_hand, villian_hand, dealer_cards):
+    river = deal_card(dealer_cards, 1)
+    display_cards(hero_hand, villian_hand, river)
+    check_hand_strength(hero_hand, villian_hand, river)
+    return river
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import cards
 
 #Create the Window
 pygame.init()
@@ -15,16 +16,6 @@ buttons = []
 def TitleScreen():
     screen.fill(BLACK)
     pygame.display.set_caption("Franklins Poker Practice - Title Screen")
-
-    button1 = button.Button("gradient_2.jpg", "water.jpg", 200, 200)
-    buttons.append(button1)
-    button2 = button.Button("gradient_2.jpg", "water.jpg", 400, 200)
-    buttons.append(button2)
-
-    
-    for button in buttons:
-        button.draw_button()
-
     titleScreen = True
     while titleScreen:
         mouse_pos = pygame.mouse.get_pos()
@@ -34,26 +25,66 @@ def TitleScreen():
                 titleScreen = False
                 pygame.quit()
             
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print(mouse_pos)
-                for button in buttons:
-                    button.checkHover()
-
 
         pygame.display.flip()
 
-    
+
+def display_cards(hero, villian, dealer_cards):
+    X = 300
+    for card in hero:
+        screen.blit(card.image, (X, 100))
+        X += 100
+        
+    X = 800
+    for card in villian:
+        screen.blit(card.image, (X, 100))
+        X += 100
+        
+    X = 400
+    for card in dealer_cards:
+        screen.blit(card.image, (X, 400))
+        X += 100
+
 
 
 def Play():
     screen.fill(BLACK)
     pygame.display.set_caption("Franklins Poker Practice - Game")
+    hero = cards.deal_player(2)
+    villian = cards.deal_player(2)
+    has_turned = False
+    has_rivered = False
+
+    # deal flop
+    flop = cards.deal_player(3)
+    display_cards(hero, villian, flop)
+
     play = True
     while play:
+        mouse_pos = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
-            run = False
-            pygame.quit()
+            if event.type == pygame.QUIT:
+                play = False
+                pygame.quit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if has_turned == False:
+                   turn = cards.deal_turn(hero, villian, flop)
+                   display_cards(hero, villian, turn)
+                   has_turned = True
+                elif has_turned == True and has_rivered == False:
+                   river = cards.deal_river(hero, villian, turn)
+                   display_cards(hero, villian, river)
+                   has_rivered = True
+                else:
+                    pass
+        
+               
+        
+
+        
+        pygame.display.flip()
 
 
-TitleScreen()
-
+Play()
